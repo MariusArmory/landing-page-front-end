ffc1f23c5c4b8c1f93097e949e7868c2c8745ea0
Timothy Marias <timothyjamesmarias@gmail.com>
2022-12-31 21:36:25 -0800

    gallery wip

---
+++ components/GalleryImage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps({
+  image: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+<template>
+  <img class="w-full h-auto rounded-lg" />
+</template>
